0280bf0dee6900362e5c45003b37cf5cb8d28de0
luizinhoCRgemid <luiz.seidel@escola.pr.gov.br>
2026-06-05 21:27:31 -0300

    Create ex01_oo.py

---
+++ python/ex01_oo.py
@@ -0,0 +1,15 @@
+ex01_oo.py
+class Produto:
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+
+produto1 = Produto("Notebook", 3500.00)
+produto2 = Produto("Mouse", 80.00)
+
+print(produto1.nome, "-", produto1.preco)
+print(produto2.nome, "-", produto2.preco)
+
+# Saída:
+# Notebook - 3500.0
+# Mouse - 80.0
