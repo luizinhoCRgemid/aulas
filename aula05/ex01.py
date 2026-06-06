34d23b1124b1c43fc68ad291a8c2f2e6d82b5847
luizinhoCRgemid <luiz.seidel@escola.pr.gov.br>
2026-06-05 21:49:15 -0300

    Update ex01.py

--- aula05/ex01.py
+++ aula05/ex01.py
@@ -1,1 +1,18 @@
+class Animal:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def comer(self):
+        print(f"{self.nome} está comendo")
+
+
+class Cachorro(Animal):
+    def latir(self):
+        print(f"{self.nome} está latindo")
+
+
+cachorro = Cachorro("Rex")
+
+cachorro.comer()
+cachorro.latir()
 
