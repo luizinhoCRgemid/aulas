4b72f268b8988fe2de6b7bb143d7225d69c3d51b
luizinhoCRgemid <luiz.seidel@escola.pr.gov.br>
2026-06-05 21:31:41 -0300

    Create ex04_oo.py

---
+++ python/ex04_oo.py
@@ -0,0 +1,30 @@
+lass ContaBancaria:
+    def __init__(self, titular, saldo):
+        self.titular = titular
+        self.saldo = saldo
+
+    def depositar(self, valor):
+        self.saldo += valor
+
+    def sacar(self, valor):
+        if valor <= self.saldo:
+            self.saldo -= valor
+        else:
+            print("Saldo insuficiente")
+
+    def extrato(self):
+        print(f"Titular: {self.titular}")
+        print(f"Saldo: R${self.saldo:.2f}")
+
+conta = ContaBancaria("Luiz Gustavo", 1000)
+
+conta.depositar(500)
+conta.sacar(300)
+conta.sacar(1500)
+
+conta.extrato()
+
+# Saída:
+# Saldo insuficiente
+# Titular: Luiz Gustavo
+# Saldo: R$1200.00
