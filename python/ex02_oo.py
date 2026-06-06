b4d1c98c612b13668e36a35b5bbf63076bb7f710
luizinhoCRgemid <luiz.seidel@escola.pr.gov.br>
2026-06-05 21:28:03 -0300

    Create ex02_oo.py

---
+++ python/ex02_oo.py
@@ -0,0 +1,15 @@
+ex02_oo.py
+class Produto:
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+
+    def desconto(self, percentual):
+        return self.preco * (1 - percentual / 100)
+
+produto = Produto("Notebook", 100.0)
+
+print(produto.desconto(10))
+
+# Saída:
+# 90.0
